d3295fff17d85e2f89fd769d77af8f219e1d7128
AsharabzyanovaAR <rustamovna14082006@gmail.com>
2024-10-16 14:49:04 +0500

    Task2

---
+++ Tyuiu.AsharabzyanovaAR.Sprint2.Task2.V23.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.AsharabzyanovaAR.Sprint2.Task2.V23.Lib;
+namespace Tyuiu.AsharabzyanovaAR.Sprint2.Task2.V23.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCheckDotInShadedArea()
+        {
+            DataService ds = new DataService();
+            int x = 4;
+            int y = 4;
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+            bool wait = true;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
